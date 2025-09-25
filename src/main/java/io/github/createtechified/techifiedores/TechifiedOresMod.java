63f54f0eafb00945b383ea538260160b94b794aa
wrldmap <123031534+wrldmap@users.noreply.github.com>
2025-09-24 19:04:16 -0500

    Make things work

---
+++ src/main/java/io/github/createtechified/techifiedores/TechifiedOresMod.java
@@ -0,0 +1,10 @@
+package io.github.createtechified.techifiedores;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("techifiedores")
+public class TechifiedOresMod {
+    public TechifiedOresMod() {
+        // Stuff will go here, eventually...
+    }
+}
