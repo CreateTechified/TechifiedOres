1e62696717ce75d19b7a8a2a95a95856b5fc4d4f
wrldmap <123031534+wrldmap@users.noreply.github.com>
2025-09-24 19:29:44 -0500

    Polished a bit

--- src/main/java/io/github/createtechified/techifiedores/TechifiedOresMod.java
+++ src/main/java/io/github/createtechified/techifiedores/TechifiedOresMod.java
@@ -2,8 +2,9 @@ package io.github.createtechified.techifiedores;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("techifiedores")
+@Mod(TechifiedOresMod.MOD_ID)
 public class TechifiedOresMod {
+    public static final String MOD_ID = "techifiedores";
     public TechifiedOresMod() {
         // Stuff will go here, eventually...
     }
